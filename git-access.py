54c4d4784f99bb70d4f656f5b18b00a6586b4274
Dipanwita-biswas-lab <85109466+Dipanwita-biswas-lab@users.noreply.github.com>
2022-07-24 01:14:01 +0530

    added doc for the git connection

---
+++ git-access.py
@@ -0,0 +1,10 @@
+'''
+1. install git  : brew install git
+2. restart pycharm
+3. Click VCS, connect through GIT
+4. Click on GIT
+5. click on Commit, add commit
+6. Click on push, Connect the repository using https
+7. for any further changes commit, comment, push
+
+'''
